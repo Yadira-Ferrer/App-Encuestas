edb49aad9d08bb7dafd8ba1c494503ac9ea7c54b
yadira-ferrer <yyfm.95@gmail.com>
2021-05-13 15:03:34 -0600

    Se añade inicio de la aplicación.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  segmentModel = 'administrador';
+
   constructor() {}
+
+  login (form) {
+    console.log(form.value);
+  }
 
 }
